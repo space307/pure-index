42064c7053a68c58331686742e4fba020752890c
Viktor <8722478+binjospookie@users.noreply.github.com>
2024-01-02 20:51:34 +0300

    chore: imp bin:

---
+++ src/bin/collectUsages.ts
@@ -0,0 +1,29 @@
+import { collectUsages as _collectUsages } from '~/collectUsages.js';
+import { getConfig } from '~/getConfig/index.js';
+import { printSet, printError, createSpinner } from '~/shared/index.js';
+
+type Params = {
+  config: Awaited<ReturnType<typeof getConfig>>;
+};
+
+const collectUsages = async ({ config }: Params) => {
+  const pkgName = config.collectUsages;
+  const spinner = createSpinner(`Collecting usages of ${pkgName}`);
+
+  // @ts-expect-error wtf
+  const result = await _collectUsages({ config });
+
+  if (result.ok) {
+    spinner.success();
+    printSet(result.val.usages);
+    process.exit(0);
+  }
+
+  spinner.error();
+  printError({
+    text: `Nothing is used from ${pkgName}. Remove it.`,
+  });
+  process.exit(1);
+};
+
+export { collectUsages };
--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,61 +1,13 @@
 #!/usr/bin/env node
 
-import { collectUsages } from '~/collectUsages.js';
 import { getConfig } from '~/getConfig/index.js';
-import { findUnusedExports } from '~/findUnusedExports.js';
-import { printSet, printError, createSpinner, readJSON } from '~/shared/index.js';
+import { collectUsages } from './collectUsages.js';
+import { findUnusedExports } from './findUnusedExports.js';
 
 const config = await getConfig();
 
 if (config.collectUsages) {
-  const pkgName = config.collectUsages;
-  const spinner = createSpinner(`Collecting usages of ${pkgName}`);
-
-  // @ts-expect-error wtf
-  const result = await collectUsages({ config });
-
-  if (result.ok) {
-    spinner.success();
-    printSet(result.val.usages);
-    process.exit(0);
-  }
-
-  spinner.error();
-  printError({
-    text: `Nothing is used from ${pkgName}. Remove it.`,
-  });
-  process.exit(1);
+  await collectUsages({ config });
 }
 
-const { name } = await readJSON('package.json');
-const pkg = { name, path: config.entry };
-const spinner = createSpinner(`Checking exports from the ${pkg.name} package`);
-
-const result = await findUnusedExports({ pkg, config });
-
-if (result.ok) {
-  spinner.success();
-  process.exit(0);
-}
-
-spinner.error();
-
-switch (result.err.reason) {
-  case 'no_exports':
-    printError({
-      text: `Nothing is exported from ${pkg.name}. Remove it.`,
-    });
-    break;
-  case 'no_imports':
-    printError({
-      text: `Nothing is imported from ${pkg.name}. Remove it.`,
-    });
-    break;
-  case 'unused_exports':
-    printError({
-      text: `Unused exports in ${pkg.name} package found`,
-      set: result.err.exports,
-    });
-}
-
-process.exit(1);
+await findUnusedExports({ config });
